51b43a182656eed09f87bbf4c6fbb755e6c5037a
Kegan Riley Edwards <keganedwards@proton.me>
2024-11-08 14:32:35 -0500

    50 current  2024-11-08 14:32:30  24.11.20241029.807e915   6.6.58                          *

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "Nixos config flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    firefox.url = "github:nix-community/flake-firefox-nightly";
+    firefox.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
